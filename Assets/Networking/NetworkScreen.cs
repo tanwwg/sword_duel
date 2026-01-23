d46d589423d9c1ce2e22d9bd6ca8cc33f8703fde
Tan Thor Jen <tthorjen@gmail.com>
2026-01-24 05:34:59 +0800

    add connect by ip

--- Assets/Networking/NetworkScreen.cs
+++ Assets/Networking/NetworkScreen.cs
@@ -1,11 +1,27 @@
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NetworkScreen : MonoBehaviour
 {
+    public TMP_InputField ipField;
+    
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
+    }
+
+    public void ConnectByIp()
+    {
+        var nm = NetworkManager.Singleton;
+        var utp = nm.GetComponent<UnityTransport>();
+
+        utp.ConnectionData.Address = ipField.text;
+        utp.ConnectionData.Port = utp.ConnectionData.Port;
+
+        nm.StartClient();  
     }
 }
